410a9304791c98ee3a2c0d9f38f27ada19f15a5a
dean <deanmeisong@gmail.com>
2024-09-24 21:53:33 +1000

    Time: 265 ms (8.82%), Space: 12.3 MB (58.82%) - LeetHub

---
+++ 1124-longest-well-performing-interval/1124-longest-well-performing-interval.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestWPI(self, hours):
+        """
+        :type hours: List[int]
+        :rtype: int
+        """
+        ans = s = 0
+        pos = {}
+        for i, x in enumerate(hours):
+            s += 1 if x > 8 else -1
+            if s > 0:
+                ans = i + 1
+            elif s - 1 in pos:
+                ans = max(ans, i - pos[s - 1])
+            if s not in pos:
+                pos[s] = i
+        return ans
