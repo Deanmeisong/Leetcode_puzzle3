2344043652b3022e3529c8cad660c89aaaa476dd
dean <deanmeisong@gmail.com>
2024-10-10 18:15:58 +1100

    Time: 50 ms (62.22%), Space: 18.1 MB (60.00%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -1,24 +1,31 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
-        def dfs(root: Optional[TreeNode], i: int):
-            if root is None:
-                return
-            dfs(root.left, i)
-            nums[i].append(root.val)
-            dfs(root.right, i)
+class Solution(object):
+    def dfs(self, root, i):
+        if root is None:
+            return
+        self.dfs(root.left, i)
+        self.nums[i].append(root.val)
+        self.dfs(root.right, i)
+    def twoSumBSTs(self, root1, root2, target):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :type target: int
+        :rtype: bool
+        """
 
-        nums = [[], []]
-        dfs(root1, 0)
-        dfs(root2, 1)
-        i, j = 0, len(nums[1]) - 1
-        while i < len(nums[0]) and ~j:
-            x = nums[0][i] + nums[1][j]
+
+        self.nums = [[], []]
+        self.dfs(root1, 0)
+        self.dfs(root2, 1)
+        i, j = 0, len(self.nums[1]) - 1
+        while i < len(self.nums[0]) and ~j:
+            x = self.nums[0][i] + self.nums[1][j]
             if x == target:
                 return True
             if x < target:
