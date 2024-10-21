8bf13f9a370a74573b453092fe45c21896475d69
dean <deanmeisong@gmail.com>
2024-10-21 21:55:42 +1100

    Time: 19 ms (74.84%), Space: 11.5 MB (93.55%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.py
@@ -6,16 +6,28 @@
        # Note that f(x, y) is increasing with respect to both x and y.
        # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
        def f(self, x, y):
-  
 """
 
-class Solution:
-    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+class Solution(object):
+    def findSolution(self, customfunction, z):
+        """
+        :type num: int
+        :type z: int
+        :rtype: List[List[int]]
+        """
         ans = []
         for x in range(1, z + 1):
-            y = 1 + bisect_left(
-                range(1, z + 1), z, key=lambda y: customfunction.f(x, y)
-            )
-            if customfunction.f(x, y) == z:
-                ans.append([x, y])
+            # Use a simple binary search implementation without bisect.
+            left, right = 1, z
+            while left <= right:
+                y = (left + right) // 2
+                value = customfunction.f(x, y)
+                if value == z:
+                    ans.append([x, y])
+                    break
+                elif value < z:
+                    left = y + 1
+                else:
+                    right = y - 1
         return ans
+        
