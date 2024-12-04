587e9a0ee0a87032279e255c3e29415ca2a4ba49
dean <deanmeisong@gmail.com>
2024-12-04 22:09:43 +1100

    Time: 100 ms (100.00%), Space: 26.4 MB (100.00%) - LeetHub

---
+++ 1564-put-boxes-into-the-warehouse-i/1564-put-boxes-into-the-warehouse-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxBoxesInWarehouse(self, boxes, warehouse):
+        """
+        :type boxes: List[int]
+        :type warehouse: List[int]
+        :rtype: int
+        """
+        n = len(warehouse)
+        left = [warehouse[0]] * n
+        for i in range(1, n):
+            left[i] = min(left[i - 1], warehouse[i])
+        boxes.sort()
+        i, j = 0, n - 1
+        while i < len(boxes):
+            while j >= 0 and left[j] < boxes[i]:
+                j -= 1
+            if j < 0:
+                break
+            i, j = i + 1, j - 1
+        return i
