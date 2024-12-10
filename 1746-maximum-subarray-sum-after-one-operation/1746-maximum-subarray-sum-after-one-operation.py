6d49deb1fceb7d794f78d8f60c42ef0d20a85daa
dean <deanmeisong@gmail.com>
2024-12-11 00:38:14 +1100

    Time: 783 ms (18.87%), Space: 30.7 MB (26.32%) - LeetHub

---
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSumAfterOperation(self, nums: List[int]) -> int:
+        f = g = 0
+        ans = -inf
+        for x in nums:
+            ff = max(f, 0) + x
+            gg = max(max(g, 0) + x, max(f, 0) + x*x)
+            f, g = ff, gg
+            ans = max(f, g, ans)
+        return ans
